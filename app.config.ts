ba7ca7de5b8bad99b77ca884466ec0898ffe8d02
Frosch79 <frosch.79.photo@gmail.com>
2025-11-26 13:18:32 +0100

    Initialized

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+import { type ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  extra: {
+    eas: {
+      projectId: 'e2cda16f-21b8-40e6-83bb-58958c5867ca',
+    },
+  },
+};
+
+export default config;
