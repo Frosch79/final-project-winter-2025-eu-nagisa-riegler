e9751f0bacc630aeba9c2fa893c17562e6773adc
Frosch79 <frosch.79.photo@gmail.com>
2025-12-18 09:48:56 +0100

    Fix vercel config

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,10 @@
+/* import path from 'node:path';
+import { createRequestHandler } from '@expo/server/adapter/vercel';
+
+export default createRequestHandler({
+  build: path.join(__dirname, '../dist/server'),
+}); */
+
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { createRequestHandler } from '@expo/server/adapter/vercel';
