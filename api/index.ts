6fced0549eaa8df6627d8ce50e5065ddc6c63de3
Frosch79 <frosch.79.photo@gmail.com>
2025-12-18 10:03:09 +0100

    Modify api path

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
-/* import path from 'node:path';
+import path from 'node:path';
 import { createRequestHandler } from '@expo/server/adapter/vercel';
 
 export default createRequestHandler({
   build: path.join(__dirname, '../dist/server'),
-}); */
+});
 
-import { dirname, join } from 'node:path';
+/* import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { createRequestHandler } from '@expo/server/adapter/vercel';
 
@@ -13,4 +13,4 @@ const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default createRequestHandler({
   build: join(currentDir, '../dist/server'),
-});
+}); */
