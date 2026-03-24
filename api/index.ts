1de46ddd204bab86d426b47911a65c828b791de6
Frosch79 <frosch.79.photo@gmail.com>
2026-03-24 12:43:44 +0100

    Modify ci test

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,9 @@
 import path from 'node:path';
 import { createRequestHandler } from '@expo/server/adapter/vercel';
 
+if (process.env.CI) {
+  process.env.EXPO_PUBLIC_API_URL = 'http://localhost:8081';
+}
 export default createRequestHandler({
   build: path.join(__dirname, '../dist/server'),
 });
